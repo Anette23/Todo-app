8bad3c10531bfab4da80195ff23ddeb389f27137
Aneta Krajčovicová <anet.krajcovicovie@gmail.com>
2023-03-06 11:18:45 +0100

    add const taskElementMarkup for markup our li

--- app.js
+++ app.js
@@ -31,4 +31,22 @@ form.addEventListener("submit", (e) => {
 function createTask(task) {
     const taskElement = document.createElement('li')
     taskElement.setAttribute('id', task.id)
+
+    const taskElementMarkup = `
+    <div>
+        <input type="checkbox" name="task-name" id="${task.id}" 
+        ${task.isCompleted ? "checked" : ""}>
+        <span ${!task.isCompleted ? "contenteditable" : ""}>${
+task.name
+}</span>
+    </div>
+    <button title="Remove ${task.name} task" class="remove-task">
+        <svg viewBox="0 0 24 24" fill="none">
+        <path d="M17.25 17.25L6.75 6.75" stroke="hsl(234, 11%, 52%)" stroke-width="1.5" stroke-linecap="round"
+        stroke-linejoin="round" />
+        <path d="M17.25 6.75L6.75 17.25" stroke="hsl(234, 11%, 52%)" stroke-width="1.5" stroke-linecap="round"
+        stroke-linejoin="round" />
+        </svg>
+    </button> 
+`;
 }
