7d820af82659e9f4fce5bca8e1d4e270f0af46b4
Aneta Krajčovicová <anet.krajcovicovie@gmail.com>
2023-03-06 10:48:19 +0100

    add variable tasks where we will save the tasks

--- app.js
+++ app.js
@@ -3,8 +3,12 @@
 const form = document.querySelector('#todo-form')
 const formInput = document.querySelector('#form-input')
 
+let tasks = []
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const inputValue = formInput.value
+
+
 })
